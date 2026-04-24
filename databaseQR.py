c97112657e35714729562f324483f322183dbc9d
alex1222797 <mauricioalexandergarciacornejo@gmail.com>
2026-04-23 22:45:04 -0600

    Update databaseQR.py

--- databaseQR.py
+++ databaseQR.py
@@ -1,29 +1,27 @@
 import pymysql
-import os # Importante para leer las variables de Render
+import os
+from urllib.parse import urlparse
 
 def conectar():
-    # Buscamos la URL de Railway en las variables de entorno de Render
-    # Si no existe (porque estás en tu PC), usará los datos de localhost
-    db_url = os.environ.get('DATABASE_URL')
-
-    if db_url:
-        # Si estamos en Render, nos conectamos usando la URL de Railway
-        # pymysql puede manejar la conexión mediante la URL o sus partes
-        # Aquí te lo pongo desglosado para que sea más seguro:
-        conexion = pymysql.connect(
-            host="shinkansen.proxy.rlwy.net",
-            port=36073,
-            user="root",
-            passwd="EcNAVDTvwiYvMSBoXpBDzxvpPecsbry",
-            database="railway"
+    # Intentar leer la URL de la variable de Render
+    url_str = os.environ.get('DATABASE_URL')
+    
+    if url_str:
+        # Esto separa automáticamente usuario, password, host y puerto
+        url = urlparse(url_str)
+        return pymysql.connect(
+            host=url.hostname,
+            port=url.port,
+            user=url.username,
+            passwd=url.password,
+            database=url.path[1:], # Quita el '/' del nombre de la base
+            connect_timeout=10
         )
     else:
-        # Si estás en tu PC (Workbench local), sigue usando esto:
-        conexion = pymysql.connect(
+        # Conexión local para tu PC
+        return pymysql.connect(
             host="localhost",
             user="root",
-            passwd="mySQL123", # Tu clave local
+            passwd="mySQL123",
             database="SistemaMedicoQR"
         )
-    
-    return conexion
